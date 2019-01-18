6b72cf25cbd65a5e6841e82ca6b4e0242a960133
Lucas Bader <lucas.bader@sap.com>
2019-01-18 09:35:33 +0100

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,26 @@
 echo "Installing dependencies"
-apt-get install tmux zsh git clang llvm-dev cmake python2.7-dev python-all-dev python-pip build-essential radare2
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sudo apt-get install tmux zsh git clang llvm-dev cmake python2.7-dev python-all-dev python-pip build-essential radare2
+
+echo "Building and installing vim"
+sudo apt-get remove vim
+cd ~
+git clone https://github.com/vim/vim.git
+cd vim
+./configure --with-features=huge \
+        --enable-multibyte \
+	    --enable-rubyinterp=yes \
+	    --enable-pythoninterp=yes \
+	    --with-python-config-dir=/usr/lib/python2.7/config \
+	    --enable-python3interp=yes \
+	    --with-python3-config-dir=/usr/lib/python3.5/config \
+	    --enable-perlinterp=yes \
+	    --enable-luainterp=yes \
+        --enable-gui=gtk2 \
+        --enable-cscope \
+	    --prefix=/usr/local
+
+make VIMRUNTIMEDIR=/usr/local/share/vim/vim81
+sudo make install
 
 echo "Building cquery"
 git clone --recursive https://github.com/cquery-project/cquery.git
@@ -9,15 +29,15 @@ git submodule update --init
 mkdir build && cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=release -DCMAKE_EXPORT_COMPILE_COMMANDS=YES
 cmake --build .
-cmake --build . --target install
+sudo cmake --build . --target install
 
 echo "Installing python dependencies"
-pip install python-language-server
+sudo pip install python-language-server
 
 cd ~
 echo "Downloading and installing configurations"
 git clone https://github.com/n3ko1/vim-rc
-mv ~/vim-rc ~/rad.vim
+mv ~/vim-rc ~/.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -33,7 +53,7 @@ git clone https://github.com/n3ko1/gdb-dashboard
 mv ~/gdb-dashboard/.gdbinit ~/.gdbinit
 rm -r ~/gdb-dashboard
 
-echo "Setting up zsh as the default shell"
-chsh -s /bin/zsh 
+echo "Oh-my-zsh installation"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 echo "Done."
